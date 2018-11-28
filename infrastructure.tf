8f18821fb1be4d2fea382c9dbd9554362a020c16
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-11-28 13:50:32 +0000

    ignore and infra

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,8 +1,10 @@
+# TODO Comment 2-3 sentences.
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   region                  = "us-east-1"
 }
 
+# TODO Comment 2-3 sentences.
 resource "aws_security_group" "game_security_group" {
   name   = "GameSecurityGroup"
 
@@ -28,6 +30,7 @@ resource "aws_security_group" "game_security_group" {
   }
 }
 
+# TODO Comment 2-3 sentences.
 resource "aws_instance" "game_server" {
   ami                    = "ami-0ac019f4fcb7cb7e6"
   instance_type          = "t2.micro"
@@ -36,14 +39,37 @@ resource "aws_instance" "game_server" {
   tags {
     Name = "GameServer"
   }
+  # TODO Comment 1-2 sentences.
+  provisioner "file" {
+    source      = "scripts/initialize_game_api_instance.sh"
+    destination = "/home/ubuntu/initialize_game_api_instance.sh"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = "${file("~/.aws/GameKeyPair.pem")}"
+    }
+  }
+  # TODO Comment 1-2 sentences.
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "/home/ubuntu/docker-compose.yml"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = "${file("~/.aws/GameKeyPair.pem")}"
+    }
+  }
   # This is used to run commands on the instance we just created.
   # Terraform does this by SSHing into the instance and then executing the commands.
   # Since it can take time for the SSH agent on machine to start up we let Terraform
   # handle the retry logic, it will try to connect to the agent until it is available
   # that way we know the instance is available through SSH after Terraform finishes.
+  # TODO Comment 1-2 sentences.
   provisioner "remote-exec" {
     inline = [
-      "# Managed to SSH into instance",
+      "chmod +x /home/ubuntu/initialize_game_api_instance.sh",
     ]
 
     connection {
@@ -54,6 +80,7 @@ resource "aws_instance" "game_server" {
   }
 }
 
+# TODO Comment 1-2 sentences.
 output "public_ip" {
   value = "${aws_instance.game_server.public_ip}"
 }

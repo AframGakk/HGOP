42e2494d816c73303e6493a2f31c38a0a0ec408d
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-11-28 12:25:14 +0000

    infrastructure changed and node_modules added to gitignore

--- infrastructure.tf
+++ infrastructure.tf
@@ -3,10 +3,57 @@ provider "aws" {
   region                  = "us-east-1"
 }
 
+resource "aws_security_group" "game_security_group" {
+  name   = "GameSecurityGroup"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "game_server" {
-  ami           = "ami-0ac019f4fcb7cb7e6"
-  instance_type = "t2.micro"
+  ami                    = "ami-0ac019f4fcb7cb7e6"
+  instance_type          = "t2.micro"
+  key_name               = "GameKeyPair"
+  vpc_security_group_ids = ["${aws_security_group.game_security_group.id}"]
   tags {
     Name = "GameServer"
   }
+  # This is used to run commands on the instance we just created.
+  # Terraform does this by SSHing into the instance and then executing the commands.
+  # Since it can take time for the SSH agent on machine to start up we let Terraform
+  # handle the retry logic, it will try to connect to the agent until it is available
+  # that way we know the instance is available through SSH after Terraform finishes.
+  provisioner "remote-exec" {
+    inline = [
+      "# Managed to SSH into instance",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = "${file("~/.aws/GameKeyPair.pem")}"
+    }
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.game_server.public_ip}"
 }

e973938a2005ede89dbf8ca0f261a2339247bb90
villi <vilhjalmur12@ru.is>
2018-12-03 15:13:27 +0000

    user repo dockercloud

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,6 +3,6 @@
 GIT_COMMIT=$1
 
 cd itemrepository
-docker build -t username/repo:$GIT_COMMIT .
+docker build -t villirn/hgop:$GIT_COMMIT .
 
 # TODO exit on error if any command fails
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,6 +2,6 @@
 
 GIT_COMMIT=$1
 
-docker push username/repo:$GIT_COMMIT
+docker push villirn/hgop:$GIT_COMMIT
 
 # TODO exit on error if any command fails

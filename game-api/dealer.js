d0c4613aa4e03744535abab608e5e50398ca5536
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-12-04 12:34:52 +0000

    dealer,deck and lucky21 game api

---
+++ game-api/dealer.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+    return {
+        shuffle: (deck) => {
+            for (let i = 0; i < deck.length - 1; i++) {
+                const j = Math.floor(Math.random() * (deck.length - i)) + i;
+                const card = deck[j];
+                const old = deck[i];
+                deck[i] = card;
+                deck[j] = old;
+            }
+        },
+        draw: (deck) => {
+            const card = deck.pop();
+            return card;
+        },
+    };
+};
---
+++ game-api/lucky21.js
@@ -0,0 +1,60 @@
+const deckConstructor = require('./deck.js');
+const dealerConstructor = require('./dealer.js');
+
+module.exports = () => {
+    let deck = deckConstructor();
+    let dealer = dealerConstructor();
+    dealer.shuffle(deck);
+    let card0 = dealer.draw(deck);
+    let card1 = dealer.draw(deck);
+    let state = {
+        deck: deck,
+        dealer: dealer,
+        cards: [
+            card0,
+            card1,
+        ],
+        // The card that the player thinks will exceed 21.
+        card: undefined,
+    };
+    return {
+        state: state,
+        // Is the game over (true or false).
+        // Is the game finished.
+        isGameOver: (game) => {
+            // TODO
+        },
+        // Has the player won (true or false).
+        playerWon: (game) => {
+            // TODO
+        },
+        // The highest score the cards can yield without going over 21 (integer).
+        getCardsValue: (game) => {
+            // TODO
+        },
+        // The value of the card that should exceed 21 if it exists (integer or undefined).
+        getCardValue: (game) => {
+            // TODO
+        },
+        // The cards value + the card value if it exits (integer).
+        getTotal: (game) => {
+            // TODO
+        },
+        // The player's cards (array of strings).
+        getCards: (game) => {
+            // TODO
+        },
+        // The player's card (string or undefined).
+        getCard: (game) => {
+            // TODO
+        },
+        // Player action (void).
+        guess21OrUnder: (game) => {
+            // TODO
+        },
+        // Player action (void).
+        guessOver21: (game) => {
+            // TODO
+        },
+    };
+};

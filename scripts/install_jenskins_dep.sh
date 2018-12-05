379ae1d2b1ea19d8ceed9b2102748792799794fb
villi <vilhjalmur12@ru.is>
2018-12-05 11:57:49 +0000

    yarn install was off

--- scripts/install_jenskins_dep.sh
+++ scripts/install_jenskins_dep.sh
@@ -56,6 +56,6 @@ if ! type yarn > /dev/null; then
     echo Installing yarn
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt updatesudo apt install yarn
+    sudo apt-get install yarn
 fi
 

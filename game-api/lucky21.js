0e35b93525d62a57b745345361b28dd8831cd32d
villi <vilhjalmur12@ru.is>
2018-12-05 13:28:06 +0000

    auto-indent 2 lines

--- game-api/lucky21.js
+++ game-api/lucky21.js
@@ -1,124 +1,124 @@
 module.exports = (deck, dealer) => {
-    dealer.shuffle(deck);
-    let card0 = dealer.draw(deck);
-    let card1 = dealer.draw(deck);
+  dealer.shuffle(deck);
+  let card0 = dealer.draw(deck);
+  let card1 = dealer.draw(deck);
 
-    let state = {
-        deck: deck,
-        dealer: dealer,
-        cards: [
-            card0,
-            card1,
-        ],
-        // The card that the player thinks will exceed 21.
-        card: undefined,
-        choice: undefined
-    };
+  let state = {
+    deck: deck,
+    dealer: dealer,
+    cards: [
+      card0,
+      card1,
+    ],
+    // The card that the player thinks will exceed 21.
+    card: undefined,
+    choice: undefined
+  };
 
 
 
-    let calcCard = (card) => {
-        let number = card.substring(0, 2);
+  let calcCard = (card) => {
+    let number = card.substring(0, 2);
 
-        if(parseInt(number) < 10 && parseInt(number) > 1) {
-            return parseInt(number);
-        } else if (parseInt(number) == 1) {
-            //let sum = calcCardAceHelper();
-            //if ((sum + 11) > 21) {
-            //    return 1;
-            //} else {
-            return 11;
-            //}
+    if(parseInt(number) < 10 && parseInt(number) > 1) {
+      return parseInt(number);
+    } else if (parseInt(number) == 1) {
+      //let sum = calcCardAceHelper();
+      //if ((sum + 11) > 21) {
+      //    return 1;
+      //} else {
+      return 11;
+      //}
+    } else {
+      return 10;
+    }
+  };
+
+  let getHandSum = () => {
+    let sum = 0;
+    state.cards.forEach((item) => {
+      sum += calcCard(item);
+    });
+    return sum;
+  };
+
+  return {
+    state: state,
+    // Is the game over (true or false).
+    isGameOver: (game) => {
+      return getHandSum() > 21;
+    },
+    // Has the player won (true or false).
+    playerWon: (game) => {
+      if(game.state.choice == 0) {
+        if(getHandSum() <= 21) {
+          return true;
         } else {
-            return 10;
+          return false;
         }
-    };
+      } else {
+        if(getHandSum() > 21) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    // The highest score the cards can yield without going over 21 (integer).
+    getCardsValue: (game) => {
+      let sum = getHandSum();
 
-    let getHandSum = () => {
-        let sum = 0;
-        state.cards.forEach((item) => {
-            sum += calcCard(item);
-        });
-        return sum;
-    };
+      if (sum >= 21) {
+        return 0;
+      }
 
-    return {
-        state: state,
-        // Is the game over (true or false).
-        isGameOver: (game) => {
-            return getHandSum() > 21;
-        },
-        // Has the player won (true or false).
-        playerWon: (game) => {
-            if(game.state.choice == 0) {
-                if(getHandSum() <= 21) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                if(getHandSum() > 21) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        // The highest score the cards can yield without going over 21 (integer).
-        getCardsValue: (game) => {
-            let sum = getHandSum();
+      for (let i = 1; i < 11; i++) {
+        if((sum + i) == 21) {
+          return 21;
+        }
+      }
 
-            if (sum >= 21) {
-                return 0;
-            }
+      return (sum + 11);
+    },
+    // The value of the card that should exceed 21 if it exists (integer or undefined).
+    getCardValue: (game) => {
+      let sum = getHandSum();
+      if (sum >= 21) {
+        return undefined;
+      }
 
-            for (let i = 1; i < 11; i++) {
-                if((sum + i) == 21) {
-                    return 21;
-                }
-            }
+      for (let i = 1; i < 11; i++) {
+        if((sum + i) == 22) {
+          return i;
+        }
+      }
 
-            return (sum + 11);
-        },
-        // The value of the card that should exceed 21 if it exists (integer or undefined).
-        getCardValue: (game) => {
-            let sum = getHandSum();
-            if (sum >= 21) {
-                return undefined;
-            }
-
-            for (let i = 1; i < 11; i++) {
-                if((sum + i) == 22) {
-                    return i;
-                }
-            }
-
-            return 11;
-        },
-        getTotal: (game) => {
-            return getHandSum();
-        },
-        // The player's cards (array of strings).
-        getCards: (game) => {
-          return game.state.cards;
-        },
-        // The player's card (string or undefined).
-        getCard: (game) => {
-            return game.state.card;
-        },
-        // Player action (void).
-        guess21OrUnder: (game) => {
-            let card = dealer.draw(game.state.deck);
-            game.state.cards.push(card);
-            game.state.card = card;
-            game.state.choice = 0;
-        },
-        // Player action (void).
-        guessOver21: (game) => {
-            let card = dealer.draw(game.state.deck);
-            game.state.cards.push(card);
-            game.state.card = card;
-            game.state.choice = 1;
-        },
-    };
+      return 11;
+    },
+    getTotal: (game) => {
+      return getHandSum();
+    },
+    // The player's cards (array of strings).
+    getCards: (game) => {
+      return game.state.cards;
+    },
+    // The player's card (string or undefined).
+    getCard: (game) => {
+      return game.state.card;
+    },
+    // Player action (void).
+    guess21OrUnder: (game) => {
+      let card = dealer.draw(game.state.deck);
+      game.state.cards.push(card);
+      game.state.card = card;
+      game.state.choice = 0;
+    },
+    // Player action (void).
+    guessOver21: (game) => {
+      let card = dealer.draw(game.state.deck);
+      game.state.cards.push(card);
+      game.state.card = card;
+      game.state.choice = 1;
+    },
+  };
 };

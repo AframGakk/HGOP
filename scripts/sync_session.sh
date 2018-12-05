cc97a6300c34531ebe3af8e061e30fec5ecaa7ff
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-12-05 14:38:51 +0000

    gitignore syns_session

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JENKINS_URL=
+JENKINS_URL=http://ec2-52-23-255-13.compute-1.amazonaws.com:8080
 
 scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"

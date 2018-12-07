22f7bf2c9672e45489d8f6ed7c5365a1b112eae0
villi <vilhjalmur12@ru.is>
2018-12-07 13:28:51 +0000

    checking for git_commit

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -25,10 +25,3 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 # TODO exit 1 if there is no docker-compose.yml file present.
 
-echo 'Starting the API'
-
-#sudo docker-compose up -d
-
-export GIT_COMMIT=$1
-docker-compose down
-docker-compose up -d

f6968f0f481e08b29830ce7a56a6a5e580a3f9b8
villi <vilhjalmur12@ru.is>
2018-12-07 12:08:30 +0000

    docker_compose_up.sh to production

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,12 +8,14 @@ git checkout $GIT_COMMIT
 # Delete all .tf files from /var/lib/jenkins/terraform/hgop/production
 rm /var/lib/jenkins/terraform/hgop/production/*.tf
 rm /var/lib/jenkins/terraform/hgop/production/*.yml
+rm /var/lib/jenkins/terraform/hgop/production/*.yml
 rm -r /var/lib/jenkins/terraform/hgop/production/scripts
 # Copy all .tf files from repository to /var/lib/jenkins/terraform/hgop/production
 mkdir /var/lib/jenkins/terraform/hgop/production/scripts
 cp *.tf /var/lib/jenkins/terraform/hgop/production
 cp *.yml /var/lib/jenkins/terraform/hgop/production
 cp ./scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
+cp ./scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
 
 #
 cd /var/lib/jenkins/terraform/hgop/production
@@ -27,8 +29,8 @@ terraform apply -auto-approve
 
 echo "Game API running at " + $(terraform output public_ip)
 
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./scripts/initialize_game_api_instance.sh"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./scripts/docker_compose_up.sh $GIT_COMMIT"
 
 #TODO exit on error if deployment fails.
 

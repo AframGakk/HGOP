1b05cdf3b72f5bb3ef5f8d67eecd21a804423093
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-12-07 14:22:38 +0000

    dealer context

--- game-api/dealer.js
+++ game-api/dealer.js
@@ -1,8 +1,10 @@
-module.exports = () => {
+module.exports = (context) => {
     return {
         shuffle: (deck) => {
+            const random = context('random');
             for (let i = 0; i < deck.length - 1; i++) {
-                const j = Math.floor(Math.random() * (deck.length - i)) + i;
+                const j = random.randomInt(i, deck.length);
+                //Math.floor(Math.random() * (deck.length - i)) + i;
                 const card = deck[j];
                 const old = deck[i];
                 deck[i] = card;

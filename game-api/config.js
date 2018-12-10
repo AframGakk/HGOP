0c2640dfbfb53ebe194c61f8f8807624bcade7ef
villi <vilhjalmur12@ru.is>
2018-12-10 14:35:58 +0000

    - postgres credentials in container

--- game-api/config.js
+++ game-api/config.js
@@ -2,9 +2,9 @@ module.exports = function(context) {
     return {
         // Postgres
         pgHost: process.env.POSTGRES_HOST || 'localhost',
-        pgUser: process.env.POSTGRES_USER || 'villi',
-        pgPassword: process.env.POSTGRES_PASSWORD || 'aframgakk1',
-        pgDatabase: process.env.POSTGRES_DB || 'cardgame',
+        pgUser: process.env.POSTGRES_USER || 'game',
+        pgPassword: process.env.POSTGRES_PASSWORD || '12345',
+        pgDatabase: process.env.POSTGRES_DB || 'game_database',
 
         // Port
         port: process.env.PORT || 3000,

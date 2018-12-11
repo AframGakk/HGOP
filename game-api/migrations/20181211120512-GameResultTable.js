3ee252cacbe1b6e05f1f604560ff00ad5d9ec363
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-12-11 13:26:15 +0000

    GameResultTable modified

--- game-api/migrations/20181211120512-GameResultTable.js
+++ game-api/migrations/20181211120512-GameResultTable.js
@@ -8,20 +8,25 @@ var seed;
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function(options, seedLink) {
+ exports.setup = function(options, seedLink) {
     dbm = options.dbmigrate;
     type = dbm.dataType;
     seed = seedLink;
-};
+  };
 
 exports.up = function(db) {
-    return null;
-};
+    return db.createTable("GameResult", {
+      ID: { type: "int", primaryKey: true, autoIncrement: true },
+      Won: { type: "boolean", notNull: true },
+      Score: { type: "int", notNull: true },
+      Total: { type: "int", notNull: true }
+    });
+  };
 
 exports.down = function(db) {
-    return null;
-};
+    return db.dropTable("GameResult");
+  };
 
 exports._meta = {
-    "version": 1
-};
+    version: 1
+  };

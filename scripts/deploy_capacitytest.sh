1a93ce3defe893b3df9828df18256ce03985faf7
villi <vilhjalmur12@ru.is>
2018-12-12 15:14:26 +0000

    - deployment scripts updated - new docker container for datadog

--- scripts/deploy_capacitytest.sh
+++ scripts/deploy_capacitytest.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
-cd /var/lib/jenkins/workspace/HGOP/repository
+rm -rf repository
+git clone git@github.com:AframGakk/HGOP.git repository
+cd repository
+git checkout $GIT_COMMIT
 
 rm -r /var/lib/jenkins/terraform/hgop/capacitytest
-
+pwd
 mkdir /var/lib/jenkins/terraform/hgop/capacitytest
 mkdir /var/lib/jenkins/terraform/hgop/capacitytest/scripts
 cp *.tf /var/lib/jenkins/terraform/hgop/capacitytest
 cp *.yml /var/lib/jenkins/terraform/hgop/capacitytest
-cp ./scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/capacitytest/scripts/initialize_game_api_instance.sh
-cp ./scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/capacitytest/scripts/docker_compose_up.sh
+cp ./scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/capacitytest/scripts/
+cp ./scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/capacitytest/scripts/
 
 cd /var/lib/jenkins/terraform/hgop/capacitytest
 
+# Destroying the terraform instance
+#terraform destroy -auto-approve -var environment=apitest || exit 1
+
 # Init Terraform
-terraform init
+terraform init -var environment=capacitytest || exit 1
 
 # Apply the instance to AWS via terraform
 terraform apply -auto-approve -var environment=capacitytest || exit 1
@@ -25,4 +31,4 @@ ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform o
 # Destroying the terraform instance
 terraform destroy -auto-approve -var environment=capacitytest || exit 1
 
-cd /var/lib/jenkins/workspace/HGOP
+#cd /var/lib/jenkins/workspace/HGOP
